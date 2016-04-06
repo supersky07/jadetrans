e99ce3fe05ce3c8c4f707ffcca17735edeaf9b75
supersky07 <supersky07@gmail.com>
2016-04-06 11:22:42 +0800

    fix path bug

--- index.js
+++ index.js
@@ -15,11 +15,8 @@ function doTransform (target_path) {
 	target_path = path.normalize(target_path);
 
 	if (!target_path.match(/^\//)) {
-		target_path = __dirname + '/' + target_path;
+		target_path = process.cwd() + '/' + target_path;
 	}
 
-	console.log(target_path)
-	
-	return;
 	commitFiles(target_path);
 };
